0267dd8001b682fdad78c5194d4e42592d05cfe0
“cristopher” <curibe26@aoiths.org>
2026-03-11 13:38:20 -0400

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -43,4 +43,33 @@ function displayData(dataArray) {
 						${mutant.name.firstName} ${mutant.name.lastName}
 					</p>
 
-				
+					<h6 class="fw-bold">Profile</h6>
+					<ul class="list-unstyled">
+						<li>Gender: ${mutant.profile.gender}</li>
+						<li>Eyes: ${mutant.profile.eyes}</li>
+						<li>Hair: ${mutant.profile.hair}</li>
+						<li>Height: ${mutant.profile.height}</li>
+					</ul>
+
+					<h6 class="fw-bold">Powers</h6>
+					<ul class="list-unstyled">
+						${powersList}
+					</ul>
+
+					<h6 class="fw-bold">Affiliation</h6>
+					<ul class="list-inline">
+						${affiliationList}
+					</ul>
+
+				</div>
+			</div>
+		</div>
+		`;
+
+		container.insertAdjacentHTML("beforeend", html);
+
+	});
+
+}
+
+loadDataSource();
