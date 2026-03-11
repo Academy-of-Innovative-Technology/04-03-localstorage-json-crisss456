dbb02a1e4afcb52ce9e94ba30fbc3346831ce953
“cristopher” <curibe26@aoiths.org>
2026-03-11 13:38:09 -0400

    update

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,34 @@ function loadDataSource() {
 
 	displayData(Database.data);
 }
+function displayData(dataArray) {
+
+	let container = document.querySelector(".row");
+
+	dataArray.forEach(function(mutant) {
+
+		let powersList = "";
+		mutant.powers.forEach(function(power){
+			powersList += `<li>${power}</li>`;
+		});
+
+		let affiliationList = "";
+		mutant.affiliation.forEach(function(team){
+			affiliationList += `<li class="list-inline-item badge bg-primary">${team}</li>`;
+		});
+
+		let html = `
+		<div class="col">
+			<div class="card shadow-lg h-100">
+				<img src="${mutant.image}" class="card-img-top">
+
+				<div class="card-body">
+					<h5 class="card-title text-center mb-3">
+						${mutant.name.alias}
+					</h5>
+
+					<p class="card-text text-center text-muted">
+						${mutant.name.firstName} ${mutant.name.lastName}
+					</p>
+
+				
